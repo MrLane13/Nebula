2a21af7feb9fd388cd4a447e43ac978febcaff75
Lane Mills <mrlaneiscool@gmail.com>
2017-02-18 22:17:44 -0600

    Added fluxuating Nebula, made it movable

--- scene.js
+++ scene.js
@@ -9,24 +9,66 @@ function drawScene() {
     var ctx = canvas.getContext("2d");
     var refreshCanvasInterval = 10;
 
-    var x = canvas.width/2;
-    var y = canvas.height/2;
-    var radius = 50;
+    var xNeb = canvas.width/2;
+    var yNeb = canvas.height/2;
+    var startRadius = canvas.width*(1/10);
+    var maxRadius = canvas.width*(1/8);
+    var minRadius = canvas.width*(1/12);
+
+    var dr = .2;
+    var dx = 2;
+    var dy = -2;
+    var shouldGrow = true;
 
     function draw() {
-
-        drawNebula();
+        moveNebula();
+        if(startRadius < maxRadius && shouldGrow) {
+            reDraw();
+            grow();
+            drawNebula("green");
+        } else {
+            shouldGrow = false;
+            if(startRadius > minRadius) {
+                reDraw();
+                shrink();
+                drawNebula("green");
+            } else {
+                shouldGrow = true;
+            }
+        }
 
     }
 
-    function drawNebula() {
+    function drawNebula(color) {
         ctx.beginPath();
-        ctx.arc(x,y,radius,0,Math.PI*2,false);
-        ctx.fillStyle = "green";
+        ctx.arc(xNeb,yNeb,startRadius,0,Math.PI*2,false);
+        ctx.fillStyle = color;
         ctx.fill();
         ctx.closePath();
-        //flux();
     }
+    function reDraw() {
+        ctx.clearRect(0,0,canvas.width,canvas.height);
+    }
+    function grow() {
+        startRadius += dr;
+    }
+    function shrink() {
+        startRadius += -dr;
+    }
+    function moveNebula() {
+        if(rightPressed) {
+            xNeb += dx;
+        } else if (leftPressed) {
+            xNeb += -dx;
+        }
+
+        if(upPressed) {
+            yNeb += dy;
+        } else if (downPressed) {
+            yNeb += -dy;
+        }
+    }
+
 
     setInterval(draw, refreshCanvasInterval);
 }
