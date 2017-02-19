fff944aba4a39bba76106b9faebbd6ee0b88c46f
Lane Mills <mrlaneiscool@gmail.com>
2017-02-18 23:10:51 -0600

    Added left paddle with spacebar “jump”

--- scene.js
+++ scene.js
@@ -15,27 +15,23 @@ function drawScene() {
     var maxRadius = canvas.width*(1/8);
     var minRadius = canvas.width*(1/12);
 
+    var widthLeftPaddle = canvas.width*(1/16);
+    var heightLeftPaddle = canvas.height*(1/4);
+    var xLeftPaddle = 2;
+    var yLeftPaddle = canvas.height/2 - (heightLeftPaddle/2);
+    var xLeftPaddleMaxJump = canvas.width*(1/12);
+    var xLeftPaddleStart = 2;
+    var shouldAdvance = false;
+    var isJumping = false;
+
     var dr = .2;
     var dx = 2;
     var dy = -2;
     var shouldGrow = true;
 
     function draw() {
-        moveNebula();
-        if(startRadius < maxRadius && shouldGrow) {
-            reDraw();
-            grow();
-            drawNebula("green");
-        } else {
-            shouldGrow = false;
-            if(startRadius > minRadius) {
-                reDraw();
-                shrink();
-                drawNebula("green");
-            } else {
-                shouldGrow = true;
-            }
-        }
+        spawnNebula();
+        spawnPaddle();
 
     }
 
@@ -55,6 +51,26 @@ function drawScene() {
     function shrink() {
         startRadius += -dr;
     }
+    // Handle all the nebula stuff
+    function spawnNebula() {
+        // moveNebula();
+        // Make the nebula's radius increase
+        if(startRadius < maxRadius && shouldGrow) {
+            reDraw();
+            grow();
+            drawNebula("green");
+        } else {
+            shouldGrow = false;
+            // Make the nebula's radius decrease
+            if(startRadius > minRadius) {
+                reDraw();
+                shrink();
+                drawNebula("green");
+            } else {
+                shouldGrow = true;
+            }
+        }
+    }
     function moveNebula() {
         if(rightPressed) {
             xNeb += dx;
@@ -67,6 +83,45 @@ function drawScene() {
         } else if (downPressed) {
             yNeb += -dy;
         }
+    }
+
+    function drawLeftPaddle() {
+        ctx.beginPath();
+        ctx.rect(xLeftPaddle, yLeftPaddle, widthLeftPaddle, heightLeftPaddle);
+        ctx.strokeStyle = "rgba(0, 0, 255, 0.5)";
+        ctx.stroke();
+        ctx.closePath();
+    }
+    function movePaddle() {
+        if(upPressed) {
+            yLeftPaddle += dy;
+        } else if (downPressed) {
+            yLeftPaddle += -dy;
+        }
+        if(spacePressed) {
+            shouldAdvance = true;
+            paddleJump();
+        } else if(!spacePressed && isJumping) {
+            paddleJump();
+        }
+
+        function paddleJump() {
+            if(xLeftPaddle < xLeftPaddleMaxJump && shouldAdvance) {
+                isJumping = true;
+                xLeftPaddle += dx;
+            } else {
+                shouldAdvance = false;
+                if(xLeftPaddle > xLeftPaddleStart) {
+                    xLeftPaddle += -dx;
+                } else {
+                    isJumping = false;
+                }
+            }
+        }
+    }
+    function spawnPaddle() {
+        movePaddle();
+        drawLeftPaddle();
     }
 
 
